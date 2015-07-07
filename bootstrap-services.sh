0b304e0ab98f13fe4e05fa7746d787c1fbf3434f
ruben <ruben.git@r.z11.de>
2015-07-08 01:10:47 +0200

    added fffmap-backend to service machine

--- bootstrap-services.sh
+++ bootstrap-services.sh
@@ -30,13 +30,14 @@ echo 'APT::Install-Recommends "0" ; APT::Install-Suggests "0" ; '>>/etc/apt/apt.
 export DEBIAN_FRONTEND=noninteractive
 
 # comment this out, if you want to keep manuals, documentation and all locales in your machines
-source $SCRIPTPATH/minify_debian.sh
+#source $SCRIPTPATH/minify_debian.sh
 
 apt-get update
 apt-get install --no-install-recommends -y \
-        puppet git tcpdump mtr-tiny vim \
+        git tcpdump mtr-tiny unp \
         openvpn tinc iptables-persistent
-
+# optional 
+#apt-get install --no-install-recommends -y puppet vim
 cd "$MACHINE_PATH"
 
 # Setup openvpn service
@@ -54,4 +55,42 @@ cp routing.conf /etc/sysctl.d/
 sysctl --system
 
 # comment this out, if you want to keep manuals, documentation and all locales in your machines
-source $SCRIPTPATH/minify_debian.sh
+#source $SCRIPTPATH/minify_debian.sh
+
+###### gateway testscript
+cd /opt/
+git clone https://github.com/rubo77/gateway-test.sh.git
+
+: "start the testscript gc-gw0 gc-gw1 and mp-gw0"
+# TODO: sed INTERFACE to eth0
+#/opt/gateway-test.sh 172.19.0.1
+
+###### Install ffmap-backend
+
+: "install alfred-json, alfred and batadv-vis"
+cat > /etc/apt/sources.list.d/draic.list << EOF
+deb [arch=amd64] http://debian.draic.info/ wheezy main
+deb-src http://debian.draic.info/ wheezy main
+EOF
+apt-get update
+# install dependencies to compile alfred:
+apt-get install libjansson-dev cmake fakeroot dpkg-dev build-essential zlib1g-dev pkg-config debhelper quilt
+cd /tmp/
+apt-get source --allow-unauthenticated alfred-json alfred
+cd alfred-json-0.3.1/
+dpkg-buildpackage -uc -us
+cd /tmp/alfred-2014.3.0/
+dpkg-buildpackage -uc -us
+dpkg -i ../alfred-json_0.3.1-1_i386.deb
+dpkg -i ../alfred_2014.3.0-11_i386.deb
+dpkg -i ../batadv-vis_2014.3.0-11_i386.deb 
+# or maybe later just
+# dpkg -i /vagrant/machines/services/alfred-json_0.3.1-1_i386.deb
+
+apt-get install rrdtool python3-pip
+pip3.2 install Networkx
+
+cd /opt/
+git clone https://github.com/ffnord/ffmap-backend.git
+mkdir -p /var/www/mesh/data/
+python3 /opt/ffmap-backend/backend.py -d /var/www/mesh/data/
